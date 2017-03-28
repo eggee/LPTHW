f5cc310efaf203d054631afec1e4cfc0badd11a5
eggee <tegg@knology.net>
2017-03-27 23:53:39 -0500

    commit frow within Pycharm, adding example 16-17 from LPTHW

---
+++ ex17_MoreFiles_condensed.py
@@ -0,0 +1,14 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+#Condensed original example down to one line.
+open(to_file, 'w').write(open(from_file).read())
+
+print "Alright, all done."
+
+# out_file.close()
+#in_file.close()
